7085d581bee573ea2349f094ae58a06dc06653cb
danis <danisahmarov@gmail.com>
2018-11-07 23:13:31 +0500

    check git2

--- simple.py
+++ simple.py
@@ -1,1 +1,13 @@
 print('hello')
+f = open("1.txt", 'r')
+while f:
+    l = f.readline()
+    if not l:
+        break
+    print(l)
+f.close()
+
+def cena(rub,kop=0):
+    return "%i руб. %i коп." % (rub,kop)
+
+print (cena(3,5))
