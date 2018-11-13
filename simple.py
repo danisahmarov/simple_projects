eabae62a4aa01dddb8819ff93b01163a35be80a4
danis <danisahmarov@gmail.com>
2018-11-13 19:08:06 +0500

    check git4

--- simple.py
+++ simple.py
@@ -7,8 +7,15 @@ while f:
     print(l)
 f.close()
 
-def cena(rub,kop=0):
+def cena(rub: object, kop: object = 0) -> object:
     return "%i руб. %i коп." % (rub,kop)
 
 print (cena(3,5))
 print (cena(2,5))
+
+D = {}
+D[1], D[2], D[3] = "ABB"
+D[0], D[1] = "AB"
+print ( len(D))
+
+print(0 < [1, 4][1] < 3 or None)
